80356dae417ff6fb5337b7c7736b5e842bfe5967
likamar <lika.marcin@gmail.com>
2024-06-26 20:25:41 +0200

    delete old files

--- Niezbednik adepta/Pakiety i moduly/zadanie_1/avg_speed_calculator.py
+++
@@ -1,2 +0,0 @@
-def calculate_avg_speed(distance, time):
-    return distance / time
--- Niezbednik adepta/Pakiety i moduly/zadanie_3/investment_calculator/calculations/future_value_calculator.py
+++
@@ -1,2 +0,0 @@
-def calculate_future_value(init_value, interest_rate, investment_years):
-    return init_value * (1 + interest_rate / 100) ** investment_years
